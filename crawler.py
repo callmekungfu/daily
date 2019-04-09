44f57208b294b09cf35551f7e7f03ff561097df7
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-04-09 12:03:40 -0400

    Closed project

--- crawler.py
+++ crawler.py
@@ -1,32 +1,38 @@
+import sys
 import scrapy
 from scrapy.http import Request
 from scrapy.http import FormRequest
 
 class BlogSpider(scrapy.Spider):
     name = 'blogspider'
-    start_urls = ['https://uottawa.brightspace.com/d2l/home']
+    
+    email = 'YOUR_EMAIL_HERE'
+    password = 'YOUR_PASSWORD_HERE'
+
+    start_urls = ['https://uottawa.brightspace.com/d2l/le/content/102575/Home']
 
     def start_requests(self):
         return [Request('https://uottawa.brightspace.com', dont_filter=True, callback=self.afterRedirect)]
 
     def parse(self, response):
-        print('\n\nParsing')
-        for title in response.css('.d2l-course-selector-item>d2l-course-selector-item-name'):
-            yield {'title': title.css('a ::text').get()}
+        print('\nParsing')
+        print(response.text)
+        for title in response.css('.d2l-navigation-s-item'):
+            print(str({'title': title.css('a ::text').get()}))
     
     def afterRedirect(self, response):
         # print(response)
-        print('\n\nAfter Redirect')
-        print(response.url,'\n\n')
+        print('\nAfter Redirect')
+        print(response.url)
         return FormRequest.from_response(
             response,
-            formdata={ 'UserName': 'ywang737@uottawa.ca', 'Password': 'Iate#62hotdogs' },
+            formdata={ 'UserName': self.email, 'Password': self.password },
             callback=self.afterLogin,
             clickdata={'id': 'submitButton'}
         )
 
     def afterLogin(self, response):
-        print('\n\nAfter login')
+        print('\nAfter login')
         print('')
         if ("Incorrect user ID or password" in response.text):
             print('Failed to login\n')
@@ -40,7 +46,6 @@ class BlogSpider(scrapy.Spider):
             
 
     def start_parsing(self, response):
-        print('\n\nParsing began:\n')
-        
+        print('\nParsing began:\n')
         yield Request('https://uottawa.brightspace.com/d2l/home')
     
---
+++ entry.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.check_output(['scrapy','runspider', 'crawler.py'])
