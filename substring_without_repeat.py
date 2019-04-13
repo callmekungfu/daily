ea13eb70d3fa62cea6462325375985eaa8b9a441
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-04-12 21:42:49 -0400

    added map based solution

--- substring_without_repeat.py
+++ substring_without_repeat.py
@@ -1,5 +1,5 @@
 # Naive
-def lengthOfLongestSubstring(s):
+def lengthOfLongestSubstringNaive(s):
     longest = 0
     if (len(s) == 1):
         return 1
@@ -14,5 +14,36 @@ def lengthOfLongestSubstring(s):
         if (length > longest):
             longest = length
     return(longest)
+
+"""
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
+"""
+
+def lengthOfLongestSubstringDictionary(s):
+    n = len(s)
+    longest = 0
+    map = {}
+    i = 0
+    for j in range(0, n):
+        if (s[j] in map):
+            i = max(map[s[j]], i)
+        longest = max(longest, j - i + 1)
+        map[s[j]] = j + 1
+    return longest
+
 while True:
-    print(lengthOfLongestSubstring(input('N test: ')))
+    print(lengthOfLongestSubstringDictionary(input('N test: ')))
