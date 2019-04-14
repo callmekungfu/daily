015c5e59a9c6a775681d0304a15d172a75a7523f
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-04-14 17:29:57 -0400

    added naive solution for longest palindrom string

--- leetcode/longest_palindrom_string.py
+++ leetcode/longest_palindrom_string.py
@@ -1,2 +1,26 @@
-def longestPalindrome(self, s):
-    while 
+# Naive (Will not work since exceeds time limit)
+def isPalindrome(s):
+    return list(s) == list(reversed(s))
+    s = s.casefold()
+
+def longestPalindrome(s):
+    if len(s) <= 1:
+        return s
+    longest = ""
+
+    for i in range(0, len(s) - 1):
+        count = ""
+        j = i + 1
+        while j < len(s):
+            if isPalindrome(s[i:j+1]):
+                count = s[i:j+1]
+            j = j + 1
+        # print(longest, count)
+        if (len(count) > len(longest)):
+            longest = count
+    return longest
+
+hello = input()
+while hello != 'bye':
+    print(longestPalindrome(hello))
+    hello = input()
