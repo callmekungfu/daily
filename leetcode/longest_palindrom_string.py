400f7eaf6ed12e827b69d720e174bcbd21e1a8b6
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-04-18 21:36:07 -0400

    Added Better version for longest palindrom string

--- leetcode/longest_palindrom_string.py
+++ leetcode/longest_palindrom_string.py
@@ -3,7 +3,7 @@ def isPalindrome(s):
     return list(s) == list(reversed(s))
     s = s.casefold()
 
-def longestPalindrome(s):
+def longestPalindromeNaive(s):
     if len(s) <= 1:
         return s
     longest = ""
@@ -19,6 +19,30 @@ def longestPalindrome(s):
         if (len(count) > len(longest)):
             longest = count
     return longest
+
+# Better Version (From Solution)
+def expandAroundCenter(s, left, right):
+    L = left
+    R = right
+    while L >= 0 and R < len(s) and s[L] == s[R]:
+        L -= 1
+        R += 1
+    return R - L - 1
+
+def longestPalindrome(s):
+    if len(s) < 1:
+        return s
+    start = 0
+    end = 0
+    for i in range(0, len(s)):
+        len1 = expandAroundCenter(s, i, i)
+        len2 = expandAroundCenter(s, i, i + 1)
+        length = max(len1, len2)
+        if (length > end - start):
+            start = i - (length - 1) // 2
+            end = i + length // 2
+    return s[start:end+1]
+
 
 hello = input()
 while hello != 'bye':
