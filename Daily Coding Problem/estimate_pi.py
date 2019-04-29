3ea21f8cfa8ebb772b84f70ab695f712aeb209db
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-04-29 19:18:15 -0400

    Added solution to monte carlo estimating py

---
+++ Daily Coding Problem/estimate_pi.py
@@ -0,0 +1,42 @@
+"""
+    Name: Implement strStr()
+    Source: Daily Coding Problem 
+    Link: https://leetcode.com/problems/implement-strstr/
+    Description:
+        This problem was asked by Google.
+        The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+        Hint: The basic equation of a circle is x2 + y2 = r2.
+"""
+
+import random
+from typing import Tuple
+
+# Imaging having 
+
+def estimatePI(radius: float) -> float:
+    estimation = 4
+    points_in = 1
+    points_total = 1
+
+    while round(estimation, 3) != 3.141:
+        point = randomPoint((-1 * radius, radius), (-1 * radius, radius), 3)
+        x = point[0]
+        y = point[1]
+        if x**2 + y**2 <= radius**2:
+            # print("inside:", str(x), str(y))
+            points_in += 1
+        else:
+            # print("outside:", str(x), str(y))
+            pass
+        points_total += 1
+        estimation = 4 * (points_in/points_total)
+        print(round(estimation, 3), "Number of Points:", points_total)
+    pass
+
+def randomPoint(x_bound: Tuple[float], y_bound: Tuple[float], accuracy: int) -> Tuple[float]:
+    x_cor = round(random.uniform(x_bound[0], x_bound[1]), accuracy)
+    y_cor = round(random.uniform(y_bound[0], y_bound[1]), accuracy)
+
+    return(x_cor, y_cor)
+
+estimatePI(1)
