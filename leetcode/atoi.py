ab58fd70dd8e295de38aa656a13e415d14e07dfe
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-05-04 20:19:19 -0500

    added partial solution for atoi

---
+++ leetcode/atoi.py
@@ -0,0 +1,35 @@
+def myAtoi(word: str) -> int:
+    stripped = word.strip().strip('+')
+    numerated = ""
+    still_number = True
+    counter = 0
+    if (len(stripped) == 0):
+        return 0
+    if (len(stripped) > 1 and stripped[0] == '-' and is_number(stripped[1])):
+        numerated = '-' + stripped[1]
+        counter = 2
+
+    while still_number and counter < len(stripped):
+        still_number = is_number(numerated + stripped[counter])
+        if (still_number):
+            numerated += stripped[counter]
+            counter += 1
+    if (len(numerated) > 0 and int(numerated) < 2147483647 and int(numerated) > -2147483648):
+        return int(numerated)
+    elif (len(numerated) > 0 and int(numerated) > 2147483647):
+        return 2147483647
+    elif (len(numerated) and int(numerated) < -2147483648):
+        return -2147483648
+    return 0
+
+def is_number(n):
+    is_number = True
+    try:
+        num = int(n)
+        # check for "nan" floats
+        is_number = num == num   # or use `math.isnan(num)`
+    except ValueError:
+        is_number = False
+    return is_number
+
+print(myAtoi('2+2'))
