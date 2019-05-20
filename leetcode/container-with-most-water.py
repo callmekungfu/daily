5dfce83fcf7f8b7a549dbb10f7be2aadda974926
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-05-19 20:19:19 -0500

    Added Brute Force Solution

--- leetcode/container-with-most-water.py
+++ leetcode/container-with-most-water.py
@@ -2,16 +2,11 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # Find Tallest Wall
-        tallest = max(height)
-        tallest_index = height.index(tallest)
-        # Find Second Tallest Wall, divide into two ways
-        height_left = height[0 : tallest_index]
-        height_right = height[tallest_index + 1 : -1]
-        tallest_left = max(height_left)
-        tallest_right = max(height_right)
-        print(tallest, tallest_index, height_left, height_right)
+        max_area: int = 0
+        for i in range(0, len(height)):
+            for j in range(i + 1, len(height)):
+                max_area = max(max_area, min(height[i], height[j]) * (j - i))
+        return max_area
 
 solution = Solution()
-print('hello')
 solution.maxArea([1,8,6,2,5,4,8,3,7])
