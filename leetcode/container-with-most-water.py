904ed763c26ee24bd9f0b104351e983e8657cfe2
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-05-20 20:19:19 -0500

    more stuff

--- leetcode/container-with-most-water.py
+++ leetcode/container-with-most-water.py
@@ -1,5 +1,12 @@
 from typing import List
 
+"""
+    Naive Solution.
+
+    Time: O(n^2)
+    Space: O(1)
+"""
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         max_area: int = 0
@@ -7,6 +14,9 @@ class Solution:
             for j in range(i + 1, len(height)):
                 max_area = max(max_area, min(height[i], height[j]) * (j - i))
         return max_area
+    
+    def maxAreaBetter(self, height: List[int]) -> int:
+        pass
 
 solution = Solution()
 solution.maxArea([1,8,6,2,5,4,8,3,7])
