a0a3cc34ddbd5a48d6f45dd8c4d3a79bc1ae2927
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-05-25 20:19:19 -0500

    added optimal solution

--- leetcode/container-with-most-water.py
+++ leetcode/container-with-most-water.py
@@ -16,7 +16,16 @@ class Solution:
         return max_area
     
     def maxAreaBetter(self, height: List[int]) -> int:
-        pass
+        max_area = 0
+        left = 0
+        right = len(height) - 1
+        while(left < right):
+            max_area = max(max_area, min(height[left], height[right]) * (right - left))
+            if (height[left] < height[right]):
+                left += 1
+            else:
+                right -= 1
+        return max_area
 
 solution = Solution()
 solution.maxArea([1,8,6,2,5,4,8,3,7])
