09222da0e45a3c900c20d5812106865cd79dcc84
Yong Lin <wangyonglin1999@gmail.com>
2020-01-09 14:46:35 -0500

    added interviewcake solution

--- interviewcake/merging_meeting_time.py
+++ interviewcake/merging_meeting_time.py
@@ -22,7 +22,31 @@ def find_end(sessions, index, target):
   if target[1] < session[0]:
     return target
   return find_end(sessions, index + 1, session)
-  
+
+def interview_cake_sol(meetings):
+  # Sort by start time
+  sorted_meetings = sorted(meetings)
+
+  # Initialize merged_meetings with the earliest meeting
+  merged_meetings = [sorted_meetings[0]]
+
+  for current_meeting_start, current_meeting_end in sorted_meetings[1:]:
+      last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
+
+      # If the current meeting overlaps with the last merged meeting, use the
+      # later end time of the two
+      if (current_meeting_start <= last_merged_meeting_end):
+          merged_meetings[-1] = (last_merged_meeting_start,
+                                  max(last_merged_meeting_end,
+                                      current_meeting_end))
+      else:
+          # Add the current meeting since it doesn't overlap
+          merged_meetings.append((current_meeting_start, current_meeting_end))
+
+  return merged_meetings
+
+# Test Runner
+
 test_cases = [
   [(0, 1), (4, 8), (3, 5), (10, 12), (9, 10)], 
   [(1, 2), (2, 3)],
