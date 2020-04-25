188a8a3427f428974a6ce4622875c3641853a0a8
Yong Lin <wangyonglin1999@gmail.com>
2020-04-25 18:07:52 -0400

    added given solution

--- interviewcake/hashing-and-hashtables/practices/in_flight_entertainment.py
+++ interviewcake/hashing-and-hashtables/practices/in_flight_entertainment.py
@@ -33,4 +33,17 @@ def has_two_movies_with_good_runtime(flight_length, movie_lengths):
       return True
   return False
 
+# Interview cake solution
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+  # Movie lengths we've seen so far
+  movie_lengths_seen = set()
+
+  for first_movie_length in movie_lengths:
+    matching_second_movie_length = flight_length - first_movie_length
+    if matching_second_movie_length in movie_lengths_seen:
+      return True
+    movie_lengths_seen.add(first_movie_length)
+
+  # We never found a match, so return False
+  return False
 print(has_two_movies_with_good_runtime(40, [20, 20, 40]))
