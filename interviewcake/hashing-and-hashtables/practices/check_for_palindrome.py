5d128ef172cfec01bdaba3e8fd26e0201c1525ae
Yong Lin <wangyonglin1999@gmail.com>
2020-04-26 18:17:39 -0400

    added my solution

---
+++ interviewcake/hashing-and-hashtables/practices/check_for_palindrome.py
@@ -0,0 +1,20 @@
+def is_palindrome(word):
+  char_map = {}
+  for char in word:
+    char_map[char] = char_map[char] + 1 if char in char_map else 1
+  if (len(word) % 2 == 1):
+    # Odd number of characters
+    found_odd = False
+    for key in char_map:
+      occurance = char_map[key]
+      if found_odd and occurance % 2 == 1:
+        return False
+      if occurance % 2 == 1:
+        found_odd = True
+  else:
+    for key in char_map:
+      occurance = char_map[key]
+      if occurance % 2 == 1:
+        return False
+  return True
+print(is_palindrome('ivicc'))
