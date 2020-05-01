f616da7f0f4efae6f8a65a51915d476b6ad40882
Yong Lin <wangyonglin1999@gmail.com>
2020-05-01 18:21:32 -0400

    added given solution

--- interviewcake/hashing-and-hashtables/practices/check_for_palindrome.py
+++ interviewcake/hashing-and-hashtables/practices/check_for_palindrome.py
@@ -21,4 +21,20 @@ def is_palindrome(word):
       if occurance % 2 == 1:
         return False
   return True
+
+# Given Solution
+# O(n)
+def has_palindrome_permutation(the_string):
+  # Track characters we've seen an odd number of times
+  unpaired_characters = set()
+
+  for char in the_string:
+    if char in unpaired_characters:
+      unpaired_characters.remove(char)
+    else:
+      unpaired_characters.add(char)
+
+  # The string has a palindrome permutation if it
+  # has one or zero characters without a pair
+  return len(unpaired_characters) <= 1
 print(is_palindrome('ivicc'))
