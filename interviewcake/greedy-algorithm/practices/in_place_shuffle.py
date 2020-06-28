957ed66b452a3dc21df9dd361e7ea835ef849170
Yong Lin <wangyonglin1999@gmail.com>
2020-06-27 21:45:59 -0400

    added my solution

--- interviewcake/greedy-algorithm/practices/in_place_shuffle.py
+++ interviewcake/greedy-algorithm/practices/in_place_shuffle.py
@@ -1,9 +1,11 @@
 '''
-Write a function for doing an in-place ↴ shuffle of a list.
+Write a function for doing an in-place shuffle of a list.
 
-The shuffle must be "uniform," meaning each item in the original list must have the same probability of ending up in each spot in the final list.
+The shuffle must be "uniform," meaning each item in the original 
+list must have the same probability of ending up in each spot in the final list.
 
-Assume that you have a function get_random(floor, ceiling) for getting a random integer that is >= floor and <= ceiling.
+Assume that you have a function get_random(floor, ceiling) for getting a random 
+integer that is >= floor and <= ceiling.
 '''
 
 import random
@@ -15,9 +17,13 @@ def get_random(floor, ceiling):
 
 def shuffle(the_list):
   # Shuffle the input in place
-
-
-  pass
+  if (len(the_list) <= 1):
+    return the_list
+  last_index_in_list = len(the_list) - 1
+  for i in range(len(the_list) - 1):
+    random_choice = get_random(i, last_index_in_list)
+    if random_choice != i:
+      the_list[i], the_list[random_choice] = the_list[random_choice], the_list[i]
 
 
 sample_list = [1, 2, 3, 4, 5]
