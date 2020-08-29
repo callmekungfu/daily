97210ed8ef1cedcc9d4908f3bcb24946b2a8d162
Yong Lin <wangyonglin1999@gmail.com>
2020-08-29 19:01:05 -0400

    add a semi working solution

--- interviewcake/hashing-and-hashtables/practices/word_cloud_data.py
+++ interviewcake/hashing-and-hashtables/practices/word_cloud_data.py
@@ -1,3 +1,5 @@
+import string
+
 '''
 You want to build a word cloud, an infographic where the size of a word 
 corresponds to how often it appears in the body of text.
@@ -17,3 +19,31 @@ your final dictionary should include one "Add" or "add" with a value of
 
 Assume the input will only contain words and standard punctuation.
 '''
+
+def make_word_map(message):
+  cleaned = split_words(message)
+  word_map = {}
+  for word in cleaned:
+    word = word.lower()
+    if word in word_map:
+      word_map[word] += 1
+    else:
+      word_map[word] = 1
+  print(word_map)
+
+def split_words(message):
+  words = []
+  word_i = 0
+  word_length = 0
+  for i, char in enumerate(message):
+    if char.isalpha():
+      if word_length == 0:
+        word_i = i
+      word_length += 1
+    else:
+      word = message[word_i:word_i + word_length]
+      words.append(word)
+      word_length = 0
+  return words
+
+make_word_map("We came, we saw, we conquered...then we ate Bill's (Mille-Feuille) cake.")
